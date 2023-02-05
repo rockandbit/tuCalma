22c3bbdf21ef5f91733a867d19cb6880e2542799
rag <raul.abuin.garcia@gmail.com>
2023-02-05 22:26:22 +0100

    no yellows

--- src/blocks/footer/Footer.js
+++ src/blocks/footer/Footer.js
@@ -1,23 +1,16 @@
-import React from 'react';
-import Copyright from '../footer/Copyright';
-import FooterMenu from '../footer/FooterMenu';
+import React from "react";
+import Copyright from "../footer/Copyright";
 
 const Footer = () => {
-    return (
-        <footer id="footer" className="site-footer">
-            <div className="wrapper no-space">
-                <div className="row justify-content-center">
-                        <Copyright />
-
-                    {/* <div className="col-xl-8 col-lg-8 align-self-center">
-                        <div className="menu float-xl-right float-lg-right">
-                            <FooterMenu />
-                        </div>
-                    </div> */}
-                </div>
-            </div>
-        </footer>
-    );
+  return (
+    <footer id="footer" className="site-footer">
+      <div className="wrapper no-space">
+        <div className="row justify-content-center">
+          <Copyright />
+        </div>
+      </div>
+    </footer>
+  );
 };
 
 export default Footer;
